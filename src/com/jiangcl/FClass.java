3a44ccabad986df2ce3eccf35fca58dc2ab1fae3
Jiang19941206 <819992593@qq.com>
2019-12-12 17:54:15 +0800

    项目创建

---
+++ src/com/jiangcl/FClass.java
@@ -0,0 +1,19 @@
+package com.jiangcl;
+
+import org.junit.Test;
+
+/**
+ * @author jiangcl
+ * @date 2019/12/12
+ * @desc
+ */
+public class FClass {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("*****************");
+    }
+}
